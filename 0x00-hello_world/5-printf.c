d73ae2c45453d8d1c105fe2993b305ab39facf6a
Abass Abiodun <abass96abiodun@gmail.com>
2022-09-11 03:56:12 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Entry point
+ * Return: always 0
  */
-int main(void) {
+int main(void) 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
