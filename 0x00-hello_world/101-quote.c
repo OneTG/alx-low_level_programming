2e60d31b4624228d4db821e220b1fd6337f94a06
Abass Abiodun <abass96abiodun@gmail.com>
2022-09-11 04:41:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - entry point
  * return: 1
  */
-int main (void)
+int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+	printf(STDOUT_FILENO, "and that piece of art is useful
 			\"-Dora Korpar, 2015-10-19\n", 59);
 	return (1)
